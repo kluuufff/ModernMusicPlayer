1dd5323fb7d5be6a1280b657d94b484851e8df04
PanteraSLS <panterasls123@gmail.com>
2024-02-20 15:00:37 +0200

    Initial Commit

---
+++ ModernMusicPlayer/ModernMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernMusicPlayerApp.swift
+//  ModernMusicPlayer
+//
+//  Created by Kluf on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ModernMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
